991861ea2b2ae25ec332e0d0e7adaf40644dbf2e
chinsun9 <40482203+chinsun9@users.noreply.github.com>
2020-11-02 15:50:01 +0900

    Feat awesome btn css

---
+++ app.js
@@ -0,0 +1,17 @@
+const btn = document.querySelectorAll('.btn');
+
+btn.forEach((item) => {
+  item.addEventListener(
+    'mouseenter',
+    function (e) {
+      const target_element = e.target;
+      let x = e.pageX - target_element.offsetLeft;
+      let y = e.pageY - target_element.offsetTop;
+
+      const sapn_element = target_element.children[0];
+      sapn_element.style.top = `${y}px`;
+      sapn_element.style.left = `${x}px`;
+    },
+    false
+  );
+});
