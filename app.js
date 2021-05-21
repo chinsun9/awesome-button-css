853796816994ac8c54670d31e2aced2f37c48ecd
chinsun9 <chin_sung@naver.com>
2021-05-21 12:58:20 +0900

    Feat mouseleave handle

--- app.js
+++ app.js
@@ -14,4 +14,18 @@ btn.forEach((item) => {
     },
     false
   );
+  
+  item.addEventListener(
+    'mouseleave',
+    function (e) {
+      const target_element = e.target;
+      let x = e.pageX - target_element.offsetLeft;
+      let y = e.pageY - target_element.offsetTop;
+
+      const sapn_element = target_element.children[0];
+      sapn_element.style.top = `${y}px`;
+      sapn_element.style.left = `${x}px`;
+    },
+    false
+  );
 });
